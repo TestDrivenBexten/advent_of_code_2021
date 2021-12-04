05208da137023cad40c0d5318be7455103cc23cf
Skyler Bexten <skyler.bexten@gmail.com>
2021-12-03 18:01:18 -0700

    Pass day 2 sample position part 1

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,27 @@
+enum class CommandType {
+    FORWARD,
+    UP,
+    DOWN
+}
+
+data class SubmarineCommand(val commandType: CommandType, val magnitude: Int)
+data class Position(val horizontal: Int, val depth: Int)
+
+private fun executeCommand(position: Position,
+                           command: SubmarineCommand): Position {
+    val x = position.horizontal
+    val y = position.depth
+    val delta = command.magnitude
+    return when(command.commandType) {
+        CommandType.FORWARD -> Position(x + delta, y)
+        CommandType.UP -> Position(x, y - delta)
+        CommandType.DOWN -> Position(x, y + delta)
+    }
+}
+
+fun moveSubmarine(commandList: List<SubmarineCommand>): Position {
+    val initialPosition = Position(0,0)
+    return commandList.fold(initialPosition) {
+            previousPosition, command -> executeCommand(previousPosition, command)
+    }
+}
