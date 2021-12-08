585d06666717f49f50ba1036403caa4c318f9c98
Skyler Bexten <skyler.bexten@gmail.com>
2021-12-07 20:21:03 -0700

    Solve for position 10

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -1,4 +1,9 @@
 
+fun calculateFuelToPosition(positionList: List<Int>, position: Int): Int {
+    val fuelSpentList = positionList.map { kotlin.math.abs(it - position) }
+    return fuelSpentList.sum()
+}
+
 fun calculateFuel(positionList: List<Int>): Int {
     return 0
 }
--- src/test/kotlin/Day7Tests.kt
+++ src/test/kotlin/Day7Tests.kt
@@ -6,6 +6,16 @@ import java.nio.file.Paths
 @DisplayName("Day 7 Tests")
 class Day7Tests {
     @Test
+    @DisplayName("Should solve fuel to position 10")
+    fun should_solve_fuel_to_position_10() {
+        val path = Paths.get("src/test/kotlin/Day7Small.txt")
+        val stringList = readStringListFromPath(path)[0].split(",")
+        val positionList = stringList.map { it.toInt() }
+        val fuelSpent = calculateFuelToPosition(positionList, 10)
+        assertEquals(71, fuelSpent)
+    }
+
+    @Test
     @DisplayName("Should solve for few crab submarines")
     fun should_solve_for_few_crab_submarines() {
         val path = Paths.get("src/test/kotlin/Day7Small.txt")
