c68331b31b05ad6e8aea2c94090d41872c1628d2
Skyler Bexten <skyler.bexten@gmail.com>
2021-12-07 20:36:56 -0700

    Solve day 7 part 2

--- src/main/kotlin/Day7.kt
+++ src/main/kotlin/Day7.kt
@@ -10,3 +10,20 @@ fun calculateFuel(positionList: List<Int>): Int {
     val fuelSpentList = (min..max).map { Pair(it, calculateFuelToPosition(positionList, it)) }
     return fuelSpentList.minByOrNull { it.second }?.second ?: 0
 }
+
+fun calculateOddFuelToPosition(positionList: List<Int>, position: Int): Int {
+    val fuelSpentList = positionList.map { it ->
+        val moveDistance = kotlin.math.abs(it - position)
+        (1..moveDistance).sum()
+    }
+    return fuelSpentList.sum()
+}
+
+fun calculateOddFuel(positionList: List<Int>): Int {
+    val min = positionList.minOrNull() ?: Int.MAX_VALUE
+    val max = positionList.maxOrNull() ?: Int.MIN_VALUE
+    val fuelSpentList = (min..max).map {
+        Pair(it, calculateOddFuelToPosition(positionList, it))
+    }
+    return fuelSpentList.minByOrNull { it.second }?.second ?: 0
+}
--- src/test/kotlin/Day7Tests.kt
+++ src/test/kotlin/Day7Tests.kt
@@ -35,4 +35,23 @@ class Day7Tests {
         assertEquals(345035, fuelSpent)
     }
 
+    @Test
+    @DisplayName("Should solve odd fuel to position 5")
+    fun should_solve_odd_fuel_to_position_5() {
+        val path = Paths.get("src/test/kotlin/Day7Small.txt")
+        val stringList = readStringListFromPath(path)[0].split(",")
+        val positionList = stringList.map { it.toInt() }
+        val fuelSpent = calculateOddFuelToPosition(positionList, 5)
+        assertEquals(168, fuelSpent)
+    }
+
+    @Test
+    @DisplayName("Should solve odd fuel for many crab submarines")
+    fun should_solve_odd_fuel_for_many_crab_submarines() {
+        val path = Paths.get("src/test/kotlin/Day7Big.txt")
+        val stringList = readStringListFromPath(path)[0].split(",")
+        val positionList = stringList.map { it.toInt() }
+        val fuelSpent = calculateOddFuel(positionList)
+        assertEquals(97038163, fuelSpent)
+    }
 }
